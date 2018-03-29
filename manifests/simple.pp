d458bfdd6f1b41947f15c07aa98751995037ff2b
ssorb <shane.sorbello@gmail.com>
2018-03-29 11:20:37 -0400

    Update simple.pp

--- manifests/simple.pp
+++ manifests/simple.pp
@@ -9,12 +9,12 @@ application wordpress_app::simple(
     database => $database,
     user     => $db_user,
     password => $db_pass,
-    export   => Database["db_${name}"]
+    export   => Database["db-${name}"]
   }
   wordpress_app::web { $name:
     apache_port => $web_port,
-    export      => Http["web_${name}"],
-    consume     => Database["db_${name}"],
+    export      => Http["web-${name}"],
+    consume     => Database["db-${name}"],
   }
 #  wordpress_app::lb { $name:
 #    balancermembers => [Http["web-${name}"]],
